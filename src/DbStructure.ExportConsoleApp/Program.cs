9f27e8487bd234dd491dcf1d0aa531a9a95dc5e7
Amael BERTEAU <amael.berteau@tech-easy.fr>
2020-07-31 19:28:13 +0200

    Adding DbStructure.ExportConsoleApp project

---
+++ src/DbStructure.ExportConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbTools.DbStructure.ExportConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
