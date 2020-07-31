19d79001e0b88ab78954176d0b53fa4e98b5f0e3
Amael BERTEAU <amael.berteau@tech-easy.fr>
2020-08-01 00:20:42 +0200

    Adding StringBuilderExtensions in Core project

---
+++ src/Core/StringBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace DbTools.Core
+{
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder AppendSqlCondition(this StringBuilder stringBuilder, string value)
+        {
+            if (!String.IsNullOrWhiteSpace(value))
+            {
+                if (stringBuilder.Length != 0)
+                    stringBuilder.Append(" AND ");
+
+                stringBuilder.Append(value);
+            }
+
+            return stringBuilder;
+        }
+    }
+}
