fe3844258b63a9213ba15f6a50bd074e9958316e
Amael BERTEAU <amael.berteau@tech-easy.fr>
2020-08-01 00:25:21 +0200

    Adding IdentifierHelper and ColumnHelper in DbStructure.Helpers project

---
+++ src/DbStructure.Helpers/ColumnHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DbTools.InformationSchema.Data;
+using DbTools.InformationSchema.Helpers;
+
+namespace DbTools.DbStructure.Helpers
+{
+    public class ColumnHelper
+    {
+        public static IEnumerable<Data.Column> GetColumns(IEnumerable<Column> columns, IEnumerable<PkColumn> pkColumns, IEnumerable<FkRefColumn> fkRefColumns, Table table)
+        {
+            IEnumerable<Column> tableColumns = columns.WhereBelongsTo(table);
+            IEnumerable<PkColumn> tablePkColumns = pkColumns.WhereBelongsTo(table).ToList();
+
+            IList<FkRefColumn> fkRefColumnList = fkRefColumns.ToList();
+
+            IList<Data.Column> columnList = new List<Data.Column>();
+
+            foreach (Column tableColumn in tableColumns)
+            {
+                Data.Column column = new Data.Column();
+                column.Name = tableColumn.ColumnName;
+                column.DataType = tableColumn.DataType;
+                column.CharacterMaximumLength = tableColumn.CharacterMaximumLength;
+                column.IsNullable = tableColumn.IsNullable.Equals("YES");
+
+                column.IsPrimaryKey = tablePkColumns.Any(c => c.ColumnName.Equals(tableColumn.ColumnName));
+
+                FkRefColumn fkRefColumn = fkRefColumnList.FirstOrDefault(c => c.ColumnName.Equals(tableColumn.ColumnName));
+                column.FkReferencedColumnIdentifier = IdentifierHelper.GetFkReferencedColumnIdentifier(fkRefColumn);
+
+                columnList.Add(column);
+            }
+
+            return columnList;
+        }
+
+
+    }
+}
---
+++ src/DbStructure.Helpers/IdentifierHelper.cs
@@ -0,0 +1,36 @@
+﻿using DbTools.DbStructure.Data;
+
+namespace DbTools.DbStructure.Helpers
+{
+    public class IdentifierHelper
+    {
+        public static TableIdentifier GetTableIdentifier(InformationSchema.Data.Table tableRow)
+        {
+            TableIdentifier tableIdentifier = new TableIdentifier();
+            tableIdentifier.Catalog = tableRow.Catalog;
+            tableIdentifier.Schema = tableRow.Schema;
+            tableIdentifier.Name = tableRow.Name;
+            return tableIdentifier;
+        }
+
+        public static ColumnIdentifier GetFkReferencedColumnIdentifier(InformationSchema.Data.FkRefColumn fkColumnDto)
+        {
+            if (fkColumnDto == null)
+                return null;
+
+            ColumnIdentifier columnIdentifier = new ColumnIdentifier();
+            columnIdentifier.TableIdentifier = GetTableIdentifier(fkColumnDto);
+            columnIdentifier.ColumnName = fkColumnDto.ReferencedColumnName;
+            return columnIdentifier;
+        }
+
+        private static TableIdentifier GetTableIdentifier(InformationSchema.Data.FkRefColumn fkColumnDto)
+        {
+            TableIdentifier tableIdentifier = new TableIdentifier();
+            tableIdentifier.Catalog = fkColumnDto.ReferencedTableCatalog;
+            tableIdentifier.Schema = fkColumnDto.ReferencedTableSchema;
+            tableIdentifier.Name = fkColumnDto.ReferencedTableName;
+            return tableIdentifier;
+        }
+    }
+}
