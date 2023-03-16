eee257c69c6cafe4cfb1a3a26c7af633d0859ce7
Amael BERTEAU <amael.berteau@tech-easy.fr>
2023-03-17 00:27:01 +0100

    [Core] Adding DataReaderExtensions

---
+++ src/Core/DataReaderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DbTools.Core
+{
+    public static class DataReaderExtensions
+    {
+        public static IEnumerable<T> ReadAll<T>(this IDataReader reader, Func<IDataReader, T> map)
+        {
+            IList<T> rows = new List<T>();
+            while (reader.Read())
+            {
+                T row = map(reader);
+                rows.Add(row);
+            }
+            return rows;
+        }
+    }
+}
