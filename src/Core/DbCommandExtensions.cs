f8ede18389439f09bb2480e8aad2efbf781fc4ad
Amael BERTEAU <amael.berteau@tech-easy.fr>
2023-03-17 00:27:35 +0100

    [Core] Adding DbCommandExtensions

---
+++ src/Core/DbCommandExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DbTools.Core
+{
+    public static class DbCommandExtensions
+    {
+        public static IEnumerable<T> GetAll<T>(this IDbCommand command, Func<IDataReader, T> map)
+        {
+            using (IDataReader reader = command.ExecuteReader())
+            {
+                IEnumerable<T> rows = reader.ReadAll(r => map(r));
+                return rows;
+            }
+        }
+    }
+}
